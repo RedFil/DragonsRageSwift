c85aef75150f75b6fc5d66bcbe39d79b982d9d62
Filipe Kunioshi <filipekunioshi@hotmail.com>
2016-02-24 00:05:50 -0800

    Implementing the change scene request in the GameViewController.

--- DragonsRageSwift/GameViewController.swift
+++ DragonsRageSwift/GameViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 import SpriteKit
 
+enum SceneName: String {
+    case Intro = "Intro"
+    case Menu = "Menu"
+    case Rank = "Rank"
+    case WorldMap = "WorldMap"
+    case BattleStage = "BattleStage"
+}
+
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -27,6 +35,9 @@ class GameViewController: UIViewController {
         scene.scaleMode = .ResizeFill
         
         skView.presentScene(scene)
+        
+        // Set up all Notification/Event listeners
+        setupListeners()
     }
 
     override func shouldAutorotate() -> Bool {
@@ -48,5 +59,105 @@ class GameViewController: UIViewController {
 
     override func prefersStatusBarHidden() -> Bool {
         return true
+    }
+    
+    /**
+     Set up all `NSNotification` listeners
+     */
+    func setupListeners() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("pressedButton:"), name: "ButtonPressed", object: nil)
+    }
+    
+    /**
+     Determines what will happen when a button is pressed according to the arguments in the `userInfo`.
+     
+     To call this method the Scene should send a `NSNotification` with the name of `ButtonPressed`.
+     
+     To config the actions, the notification should have a `userInfo` with the field `btnActionName`.
+     
+     **btnActionName** list:
+     - `ChangeScene`: For this action, the `userInfo` should also contain the name of the next scene for the key `SceneName`.
+     */
+    func pressedButton(notification: NSNotification) {
+        // Get the Button's action command
+        let btnActionName = notification.userInfo?["btnActionName"]
+        
+        // Check if it is a valid command
+        if let btnActionName = btnActionName, actionName = btnActionName as? String {
+            // Determine the Controller action according to the request from the button
+            switch(actionName) {
+                case "ChangeScene":
+                    // If it is a request to change the scene, check if the requested scene is valid
+                    if let userScene = notification.userInfo?["SceneName"] as? String, sceneName = SceneName(rawValue: userScene) {
+                        changeSceneWithName(sceneName)
+                    }
+                    break
+                default:
+                    break
+            }
+        }
+    }
+    
+    /**
+     Change the current Scene to the one corresponding to the `sceneName`.
+     
+     - parameters:
+        - sceneName: One of the registered Scene Names.
+     */
+    func changeSceneWithName(sceneName: SceneName) {
+        let transition = SKTransition.revealWithDirection(SKTransitionDirection.Down, duration: 1.0)
+        
+        let nextScene = discoverSceneByName(sceneName)
+        
+        changeSceneWithTransition(nextScene, transition: transition)
+    }
+    
+    /**
+     Change the current Scene to the given `nextScene`.
+     
+     - parameters:
+        - nextScene: One of the subclasses of `DRScene`.
+     */
+    func changeScene(nextScene: DRScene) {
+        let transition = SKTransition.revealWithDirection(SKTransitionDirection.Down, duration: 1.0)
+        
+        changeSceneWithTransition(nextScene, transition: transition)
+    }
+    
+    /**
+     Get the `DRScene` subclass corresponding to the given `sceneName`.
+     
+     - parameters:
+        - sceneName: One of the registered Scene Names.
+     
+     - returns: A `DRScene` subclass according to the given `sceneName`.
+     */
+    func discoverSceneByName(sceneName: SceneName) -> DRScene {
+        switch(sceneName) {
+            case .Intro:
+                return DRScene()
+            case .Menu:
+                return ScnMenu()
+            case .WorldMap:
+                return ScnWorldMap()
+            case .BattleStage:
+                return DRScene()
+            default:
+                return ScnMenu()
+        }
+    }
+    
+    /**
+     Indeed change the current scene to the given `nextScene` with the given `transition` animation.
+     
+     - parameters:
+        - nextScene: Which scene the game should move to.
+        - transition: The `SKTransition` animation that should happen when changing between the scenes.
+     */
+    func changeSceneWithTransition(nextScene: DRScene, transition: SKTransition) {
+        nextScene.scaleMode = .ResizeFill
+        
+        let skView = self.view as! SKView
+        skView.presentScene(nextScene, transition: transition)
     }
 }
