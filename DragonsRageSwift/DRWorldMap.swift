52178e7e123d615242ef5e0afcb29999b89a7599
Filipe Kunioshi <filipekunioshi@hotmail.com>
2016-02-25 22:15:18 -0800

    Implementing the World Map Scene with the basic map generation.

---
+++ DragonsRageSwift/DRWorldMap.swift
@@ -0,0 +1,111 @@
+//
+//  DRWorldMap.swift
+//  DragonsRageSwift
+//
+//  Created by Filipe Kunioshi on 2016-02-25.
+//  Copyright © 2016 Filipe Kunioshi. All rights reserved.
+//
+
+import SpriteKit
+
+/**
+ All the possible tile types in the World Map.
+ */
+public enum WorldMapGridType: Int {
+    case None = -999
+    
+    // Walkables
+    case Grass = 0
+    case Dirt = 2
+    case Water = 6
+    
+    // Usables (walkables)
+    case Cave = 3
+    case CastleDirt = 4
+    case CastleGrass = 5
+    
+    // Non-walkables
+    case Mountain = 1
+}
+
+class DRWorldMap {
+    /**
+     The type based world map.
+     
+     Obs.: No Sprites and/or Nodes are in here.
+     */
+    var map: [[WorldMapGridType]] = [[]]
+    
+    /**
+     The Textures dictionary for each value of `WorldMapGridType`.
+     */
+    var tileTextures: [WorldMapGridType: SKTexture] = [.None: SKTexture()]
+    
+    let worldTileSize: CGFloat
+    
+    init() {
+        // MARK: Adding which sprite each TileType should be
+        
+        // Walkables
+        tileTextures[.Grass] = SKTexture(imageNamed: "tile-grass")
+        tileTextures[.Dirt] = SKTexture(imageNamed: "tile-earth")
+        tileTextures[.Water] = SKTexture(imageNamed: "tile-water")
+        
+        // Usables (walkables)
+        tileTextures[.Cave] = SKTexture(imageNamed: "tile-cave")
+        tileTextures[.CastleDirt] = SKTexture(imageNamed: "tile-castle-earth")
+        tileTextures[.CastleGrass] = SKTexture(imageNamed: "tile-castle-grass")
+        
+        // Non-walkables
+        tileTextures[.Mountain] = SKTexture(imageNamed: "tile-mountain")
+        
+        
+        // Defining the size of the grid tiles
+        worldTileSize = tileTextures.first!.1.size().height
+    }
+    
+    func generateMap() {
+        // Temp map (Mockup)
+        let tempMap: [[Int]] =
+        [
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,2,1,1,1,2,2,2,2,2,2,1,1,1,1,1,1,1,1,2,2,1,1,1,1,1,1,1],
+            [1,1,2,3,2,2,2,2,2,0,0,0,2,2,2,2,1,1,1,1,2,2,2,2,2,1,1,1,1,1],
+            [1,1,1,2,2,2,2,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1],
+            [1,1,1,1,2,2,2,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,2,2,1,2,2,1,1,1],
+            [1,1,1,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,1,1,2,2,1,1,1],
+            [1,1,1,2,2,1,1,2,0,0,0,0,6,6,0,0,0,0,0,0,2,2,1,1,1,2,2,1,1,1],
+            [1,1,2,2,1,2,2,2,0,0,0,6,6,6,6,0,0,0,0,0,2,1,1,1,2,2,2,2,1,1],
+            [1,1,2,2,2,2,0,0,0,0,0,0,0,6,6,0,0,0,0,2,2,1,1,1,2,2,2,2,1,1],
+            [1,1,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,1,1,1,2,2,2,2,2,1,1],
+            [1,6,6,6,6,6,0,0,0,0,0,0,0,0,0,0,0,0,2,2,1,3,1,2,2,2,2,1,1,1],
+            [1,6,6,6,6,6,0,0,0,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,2,1,1,1],
+            [1,2,2,6,6,6,5,6,6,6,6,0,0,6,6,6,6,6,0,0,0,0,0,0,0,0,2,2,1,1],
+            [1,2,2,0,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,0,0,0,0,0,0,0,0,2,2,1],
+            [1,1,2,2,0,0,6,6,0,0,6,6,6,6,6,5,6,6,6,0,0,0,0,0,0,0,0,0,2,1],
+            [1,1,1,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1],
+            [1,1,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,0,0,0,0,2,2,1],
+            [1,2,2,3,1,2,0,0,0,0,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,2,2,1,1],
+            [1,2,2,1,1,2,0,0,0,0,0,0,0,0,0,2,2,1,1,1,2,0,0,0,0,0,2,2,1,1],
+            [1,1,2,2,1,2,0,0,0,0,0,0,0,0,0,2,2,1,3,2,2,0,0,0,0,0,0,2,2,1],
+            [1,1,2,2,2,6,6,6,6,0,0,0,0,0,0,2,1,2,2,2,6,6,0,0,0,0,0,2,2,1],
+            [1,1,2,2,2,6,6,6,6,6,0,0,0,0,0,2,2,2,2,6,6,6,0,0,0,0,0,2,2,1],
+            [1,1,1,2,2,2,6,6,6,2,2,2,2,0,0,0,0,6,6,6,6,0,0,0,0,0,0,2,2,1],
+            [1,1,1,2,2,2,2,2,2,2,1,1,2,2,0,0,0,6,6,6,0,0,0,0,0,2,2,2,1,1],
+            [1,1,1,2,2,2,2,2,2,2,1,1,1,2,0,0,0,0,0,0,0,0,0,2,2,2,2,2,1,1],
+            [1,1,1,2,2,2,4,2,2,2,1,1,1,2,2,0,0,0,0,0,0,2,2,2,2,2,2,2,1,1],
+            [1,1,1,1,1,2,2,2,2,1,1,1,1,1,2,2,2,0,0,2,2,2,1,2,2,2,4,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,1,1,1,1,2,2,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+        ];
+        
+        var tempMapConverted: [[WorldMapGridType]] = [[]]
+        for (index, tempRow) in tempMap.enumerate() {
+            let tempRowConverted: [WorldMapGridType] = tempRow.map( { WorldMapGridType(rawValue: $0)! } )
+            tempMapConverted.insert(tempRowConverted, atIndex: index)
+        }
+        
+        map = tempMapConverted
+    }
+}
--- DragonsRageSwift/ScnWorldMap.swift
+++ DragonsRageSwift/ScnWorldMap.swift
@@ -12,15 +12,75 @@ class ScnWorldMap: DRScene {
     /**
      The Main Character of the game (the Player itself).
      */
-    static let mainCharacter: DRCharacter = DRCharacter()
+    static let mainCharacter: DRCharacter = DRCharacter(imageNamed: "player")
+    
+    /**
+     The bases of the WorldMap grid.
+     */
+    private let worldMap: DRWorldMap = DRWorldMap()
+    
+    /**
+     The `Node` that hold the World Map Tiles as Sprite Nodes.
+     */
+    private var gridMapNode: DRSpriteNode = DRSpriteNode()
     
     override func didMoveToView(view: SKView) {
+        // Adding the main character in the scene
+        ScnWorldMap.mainCharacter.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        ScnWorldMap.mainCharacter.zPosition = 10
         
+        self.addChild(ScnWorldMap.mainCharacter)
+        
+        // Map creation
+        createLogicalMap()
+        createMapSprite()
+        gridMapNode.position.y += self.frame.height
+        
+        self.addChild(gridMapNode)
+        
+        // MARK: HUD creation
+        // TODO: Create the HUD sprite nodes
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        // TODO: Implement the interaction with the in-game menu.
     }
     
     override func update(currentTime: CFTimeInterval) {
+    }
+    
+    // MARK: Map creation
+    
+    /**
+     Creates the map as a code for future transcription to sprites.
+     */
+    func createLogicalMap() {
+        worldMap.generateMap()
+    }
+    
+    /**
+     Transcribes the World Map code based into sprite based map in the scene.
+     */
+    func createMapSprite() {
+        for (y, tileRow) in worldMap.map.enumerate() {
+            for (x, tileType) in tileRow.enumerate() {
+                let tileNode = DRSpriteNode(texture: worldMap.tileTextures[tileType])
+                tileNode.position = CGPoint(x: CGFloat(x) * worldMap.worldTileSize, y: CGFloat(-y) * worldMap.worldTileSize)
+                
+                gridMapNode.addChild(tileNode)
+            }
+        }
+    }
+    
+    /**
+     Creates a `SpriteNode` for a world map tile of the given `type` at the given grid `position`.
+     
+     - parameters:
+        - type: Which type the tile should be created.
+        - position: The tile's position in the *Code Based* World Map.
+        - tileSize: Which size one tile should be. (*Used to calculate the the new tile position according to the Code Based map*)
+     */
+    func createMapTileSprite(type: WorldMapGridType, position: CGPoint, tileSize: Float) {
+        
     }
 }
